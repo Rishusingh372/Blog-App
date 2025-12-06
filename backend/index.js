d41c4d66adc7d10b8d5ba9df4cd951d5087be540
Rishu kumar singh <rishu272018@gmail.com>
2025-12-06 11:55:09 +0530

    cloudinary updated

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
